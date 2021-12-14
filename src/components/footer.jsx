bdc53d3349b26d8ac991ea250a3cd6003f20d2c3
rent931 <92768223+rent931@users.noreply.github.com>
2021-12-13 19:57:42 -0800

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h4>Flower Plug 2021, All rights reserved</h4>
+      <h5>Miguel Renteria</h5>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,12 @@
+import "./navBar.css";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <h5>Nav menu will be here</h5>
+      <button>This is a test button</button>
+    </div>
+  );
+}
+
+export default NavBar;
